f4f3dffc10799e53d09f15b05a60fc7916859ce2
hoangquan02072002 <81171046+hoangquan02072002@users.noreply.github.com>
2023-06-21 18:32:41 +0300

    first commit

---
+++ app/config.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'e341e6698cb20dd889d040a9be7d5fc129cb06255f349bd6ea3f901afe8d61b4'
+
+MYSQL_USER = 'std_2178_lab4_211_351'
+MYSQL_PASSWORD = 'hoangquan2002'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = 'std_2178_lab4_211_351'
+ADMIN_ROLE_ID = 2
